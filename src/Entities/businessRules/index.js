145369196a28827d52efbcb3ec89cb22c1032c25
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-03-09 21:09:28 +0100

    add saveUser module

--- src/Entities/businessRules/index.js
+++ src/Entities/businessRules/index.js
@@ -1,6 +1,7 @@
 module.exports = {
-  validEmail  : require('../businessRules/validEmail'),
+  validEmail   : require('../businessRules/validEmail'),
   findUser     : require('./findUser'),
+  saveUser     : require('./saveUser'), 
   hashData     : require('../../infra').hashData,
   compareData  : require('../../infra').compareData,
   generateToken: require('./token').generateToken,
