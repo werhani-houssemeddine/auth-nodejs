b9b6315acba77d7db3a8ac200e9ea29dd7243109
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-03-09 21:04:22 +0100

    change the validation fn name and remove comments

--- src/useCases/authenticateUser.js
+++ src/useCases/authenticateUser.js
@@ -1,18 +1,18 @@
-const validateCredentials = require('../Entities/login.entity');
+const validateLoginCredentials = require('../Entities/login.entity');
 const { findUser, generateToken, compareData } = require('../Entities/businessRules');
 
 module.exports = async function (_email, _password) {
   try {
-    const { email, password } = validateCredentials(_email, _password);
+    const { email, password } = validateLoginCredentials(_email, _password);
   
-    /*const existUser = await findUser({ property: 'email', value: email });
+    const existUser = await findUser({ property: 'email', value: email });
     if(!existUser) return { status: 401, body: { message: 'Email or Password are incorects', 'a': 'a' }};
 
     const muchPassword = await compareData(existUser.password, password);
-    if(!muchPassword) return { status: 401, body: { message: 'Email or Password are incorects' }};
+    if(!muchPassword) return { status: 401, body: { message: 'Email or Password are incorects', 'p': 'p' }};
 
-    const payload = { id: existUser.id, email };*/
-    const token = generateToken({ id: 1, email });
+    const payload = { id: existUser.id, email };
+    const token = generateToken(payload);
 
     return { status: 200, body: { state: true, token, email }};
 
