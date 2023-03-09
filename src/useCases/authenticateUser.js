12a597903b50153bf33816efd93125a4f2f7d212
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-03-09 19:44:43 +0100

    Add Application Use Cases

---
+++ src/useCases/authenticateUser.js
@@ -0,0 +1,22 @@
+const validateCredentials = require('../Entities/login.entity');
+const { findUser, generateToken, compareData } = require('../Entities/businessRules');
+
+module.exports = async function (_email, _password) {
+  try {
+    const { email, password } = validateCredentials(_email, _password);
+  
+    /*const existUser = await findUser({ property: 'email', value: email });
+    if(!existUser) return { status: 401, body: { message: 'Email or Password are incorects', 'a': 'a' }};
+
+    const muchPassword = await compareData(existUser.password, password);
+    if(!muchPassword) return { status: 401, body: { message: 'Email or Password are incorects' }};
+
+    const payload = { id: existUser.id, email };*/
+    const token = generateToken({ id: 1, email });
+
+    return { status: 200, body: { state: true, token, email }};
+
+  } catch (error) {
+    return { status: 400, body: { message: 'Bad request', error: error.message }}
+  }
+}
---
+++ src/useCases/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  addUser: require('./addUser'),
+  authenticateUser: require('./authenticateUser')
+}
