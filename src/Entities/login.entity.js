e7223a6c7d4a119d698576d1d398adc30cc39351
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-03-09 21:09:07 +0100

    changed the module name

--- src/Entities/login.entity.js
+++ src/Entities/login.entity.js
@@ -1,9 +1,9 @@
 // this function will verify that the credentials are valid
 // and return an object containing { email: string, password: string }
 
-const isValidEmail = require('./businessRules/valideEmail')
+const { validEmail } = require('./businessRules/')
 
-function createCredentials(email, password) {
+module.exports = function createCredentials(email, password) {
   
   if(!email) throw new Error('Email required');
   if(!password) throw new Error('Password required');
@@ -11,11 +11,8 @@ function createCredentials(email, password) {
   const _email = email.trim()?.toLowerCase();
   const _password = password.trim();
 
-  if(!isValidEmail(email)) throw new Error('Invalid email');
+  if(!validEmail(email)) throw new Error('Invalid email');
   if(_password.length < 8) throw new Error('Invalid password, length must be more than 8');
 
   return { email: _email, password: _password };
 }
-
-
-exports.createCredentials = createCredentials;
