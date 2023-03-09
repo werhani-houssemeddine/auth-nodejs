d672d4b38dcd9428e78915e4354a71ee00ddefe0
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-03-09 07:02:29 +0100

    Add description and implementation of a function that will handle the request response

---
+++ src/makeRequest/index.js
@@ -0,0 +1,23 @@
+module.exports = function makeRequest(controller) {
+  return (req, res) => {
+    const headers = { "Content-type": "application/json" };
+
+    const httpRequest = {
+      body    : req.body,
+      params  : req.params,
+      query   : req.query,
+      method  : req.method,
+      path    : req.path,
+      headers
+    }
+    
+    controller(httpRequest)
+      .then(httpResponse => {
+        res.status(httpResponse.status).send(httpResponse.body);
+      })
+      .catch(err => {
+        console.log('oop');
+        res.status(500).send({ message: 'Internal Server Error' });
+      })
+  }
+}
